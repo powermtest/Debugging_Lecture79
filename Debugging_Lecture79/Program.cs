9a55287098e30426f01891933c1b82f45667f71f
powermtest <powermtest@gmail.com>
2018-08-03 16:40:35 +0200

    koniec na razie
    jeszcze wrócimy do tematu

--- Debugging_Lecture79/Program.cs
+++ Debugging_Lecture79/Program.cs
@@ -10,7 +10,7 @@ namespace Debugging_Lecture79
     {
         static void Main(string[] args)
         {
-            var numbers = new List<int> {1, 2, 3, 4, 5, 6};
+            var numbers = new List<int> {1, 2};
             var smallests = GetSmallests(numbers, 3);
 
             foreach (var number in smallests)
@@ -21,13 +21,19 @@ namespace Debugging_Lecture79
 
         public static List<int> GetSmallests(List<int> list, int count)
         {
+            if (count > list.Count)
+            {
+                throw new ArgumentOutOfRangeException("count", "Count nie moze byc wiekszy niz liczba elementow listy");
+                
+            }
+            var buffer = new List<int>(list);
             var smallests = new List<int>();
             
             while (smallests.Count < count)
             {
-                var min = GetSmallest(list);
+                var min = GetSmallest(buffer);
                 smallests.Add(min);
-                list.Remove(min);
+                buffer.Remove(min);
             }
             return smallests;
         }
